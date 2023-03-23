ad641c3ce926f12a70c98ca1e2543e0030ec70c1
bharatharavind7 <111413831+bharatharavind7@users.noreply.github.com>
2023-03-24 00:37:09 +0530

    Update httpd_stop.sh

--- scripts/httpd_stop.sh
+++ scripts/httpd_stop.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 # Stop Apache HTTP server
-sudo systemctl stop apache2
+systemctl stop apache2
 
 # Disable Apache HTTP server to start at boot time
-sudo systemctl disable apache2
+systemctl disable apache2
 
 # Check the status of Apache HTTP server
-sudo systemctl status apache2
+systemctl status apache2
